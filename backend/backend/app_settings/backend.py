1112d41c6d2db3727270538c2655e37c1828fa42
changwoo <ep6tri@hotmail.com>
2016-11-09 00:58:29 +0900

    LIFEMOTIF_DEFAULT_AUTH_DISH parameter added

--- backend/backend/app_settings/backend.py
+++ backend/backend/app_settings/backend.py
@@ -3,3 +3,5 @@ import os
 from backend.app_settings.common.path_settings import PROJECT_DIR
 
 LIFEMOTIF_DEFAULT_STATICFILES_DIR = os.path.join(PROJECT_DIR, 'staticfiles')
+
+LIFEMOTIF_DEFAULT_AUTH_DISH = 'backend.auth_dishes.PythonSocialAuthDish'
