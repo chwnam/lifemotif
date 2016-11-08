be41d1736f4b3f05600a7b2d9e62d71b15d154f6
changwoo <ep6tri@hotmail.com>
2016-11-09 01:09:22 +0900

    views updated.

--- backend/emails/api/v1/gmail/api_views.py
+++ backend/emails/api/v1/gmail/api_views.py
@@ -1,20 +1,19 @@
 """
 Gmail API v1 Views
 """
-from rest_framework import (
-    generics,
-    permissions
-)
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 from oauth2_provider.ext.rest_framework import TokenHasReadWriteScope, TokenHasScope
-from rest_framework.exceptions import NotAuthenticated
+from rest_framework import (generics, permissions, )
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED
 
-from backend.auth_dishes import PythonSocialAuthDish
-from emails.providers.gmail import (
-    models,
-    serializers,
-)
+from backend.auth_dishes import get_auth_dish
+from backend.models import AsyncTaskResult
+from backend.serializers import AsyncTaskResultSerializer
+from emails.tasks import update_gmail_message_index
+from emails.providers.gmail import (models, serializers, )
 from emails.providers.gmail.api import GmailApi
 
 
@@ -73,13 +72,14 @@ class LabelsView(APIView):
 
     @staticmethod
     def get(request, *args, **kwargs):
-        auth_dish = PythonSocialAuthDish(user=request.user, request=request, provider='google-oauth2')
+        auth_dish = get_auth_dish(user=request.user, request=request)
         api = GmailApi(auth_dish=auth_dish)
         labels = api.get_labels()
         return Response(labels)
 
 
 class UpdateIndexView(APIView):
+
     _ignore_model_permissions = True
 
     def _allowed_methods(self):
@@ -87,4 +87,23 @@ class UpdateIndexView(APIView):
 
     @staticmethod
     def post(request, *args, **kwargs):
-        pass
+
+        if settings.LIFEMOTIF_TASK_TYPE == 'celery':
+            async_result = update_gmail_message_index.delay(request.user.id)
+            task_result = AsyncTaskResult.objects.create(
+                task_id=async_result.task_id,
+                user=request.user,
+                job_type='gmail-update'
+            )
+            serializer = AsyncTaskResultSerializer(instance=task_result)
+
+            return Response(data=serializer.data, status=HTTP_201_CREATED)
+
+        elif settings.LIFEMOTIF_TASK_TYPE == 'direct':
+            update_gmail_message_index(request.user.id)
+            return Response(data={}, status=HTTP_201_CREATED)
+
+        else:
+            raise ImproperlyConfigured(
+                'You must declare LIFEMOTIF_TASK_TYPE in the settings, and set the value as \'celery\', or \'direct\''
+            )
