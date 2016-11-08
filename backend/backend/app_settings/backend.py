f5466f1f68e80407411e03907af7391673a27705
changwoo <ep6tri@hotmail.com>
2016-11-09 01:09:07 +0900

    LIFEMOTIF_TASK_TYPE variable added.

--- backend/backend/app_settings/backend.py
+++ backend/backend/app_settings/backend.py
@@ -5,3 +5,5 @@ from backend.app_settings.common.path_settings import PROJECT_DIR
 LIFEMOTIF_DEFAULT_STATICFILES_DIR = os.path.join(PROJECT_DIR, 'staticfiles')
 
 LIFEMOTIF_DEFAULT_AUTH_DISH = 'backend.auth_dishes.PythonSocialAuthDish'
+
+LIFEMOTIF_TASK_TYPE = 'celery'  # or use 'direct' for development
